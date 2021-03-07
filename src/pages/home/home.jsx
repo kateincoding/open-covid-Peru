1068d7c0bf58e94e35bd79e099aa6bf332c3a044
Your Name <k.soto@pucp.pe>
2021-03-07 08:23:56 -0500

    home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+/*}  Card,
+CardContent, */
+} from "@material-ui/core";
 import DownBar from "../../components/downbar/downbar";
 
 function Home(){
   return (
     <div>
+      <h1>Selecciona tu distrito:</h1>
+      <FormControl className="app_drop_down">
+        <Select
+              variant="outlined"
+              value = "abc"
+            >
+              <MenuItem MenuItem value="Distrito">Lima</MenuItem>
+              <MenuItem MenuItem value="Distrito">Ica</MenuItem>
+              <MenuItem MenuItem value="Distrito">Piura</MenuItem>
+
+           {/* value={index}
+              onChange={onIndexChange} 
+            >
+              <MenuItem value="index">Worldwide</MenuItem>
+              {countries.map((index) => (
+              <MenuItem value={index.value}>{index.name}</MenuItem> 
+              ))} */}
+
+        </Select>
+      </FormControl>
       <DownBar/>
     </div>
   );
