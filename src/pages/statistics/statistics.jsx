d04eb2e45e9d83a77bf0028bf22293603148ed56
Your Name <k.soto@pucp.pe>
2021-03-07 05:13:18 -0500

    dropbox

--- src/pages/statistics/statistics.jsx
+++ src/pages/statistics/statistics.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 import DownBar from "../../components/downbar/downbar";
 
 function Statistics(){
   return (
     <div>
+      <FormControl className="app_drop_down">
+        <Select
+              variant="outlined"
+              value = "abc"
+            >
+              <Menu MenuItem value="Fallecidos/día"> </Menu>
+              <Menu MenuItem value="Vacunados/día"> </Menu>
+              <Menu MenuItem value="UCI/día"> </Menu>
+
+           {/* value={index}
+              onChange={onIndexChange} 
+            >
+              <MenuItem value="index">Worldwide</MenuItem>
+              {countries.map((index) => (
+              <MenuItem value={index.value}>{index.name}</MenuItem> 
+              ))} */}
+
+        </Select>
+      </FormControl>
       <DownBar/>
     </div>
   );
